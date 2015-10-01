2c3ed9302b3b1c14f7a8b443642bfb7f82afef3a
azeemgolive <azeem@golive.com.pk>
2015-10-01 16:30:39 +0500

    code for easy paisa payment

--- payment-easypaisa.php
+++ payment-easypaisa.php
@@ -1,8 +1,10 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+session_start();
+include("dbconnection.php");
+if(isset($_GET['orderRefNum']))
+$order_number = $_GET['orderRefNum'];// geting order number
+$createdAt=  date('Y-m-d');
+$updatedAt=  date("Y-m-d");  
+$query="insert into easypaisa_payment(order_number,createdAt,updatedAt) values('$order_number','$createdAt','$updatedAt')";
+mysql_query($query) or die(mysql_error())
+?>
